8d21347f97db90a44fa379b1f8b6edaed0ffe0b3
Hackbright Student <info@hackbrightacademy.com>
2016-04-26 18:07:50 -0700

    edits to /students route

--- hackbright-web.py
+++ hackbright-web.py
@@ -11,10 +11,13 @@ def get_student():
 
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
+    project_title, grade = hackbright.get_grades_by_github(github)
     html = render_template("student_info.html",
                             first=first,
                             last=last,
-                            github=github)
+                            github=github,
+                            project_title=project_title,
+                            grade=grade)
     return html
 
 @app.route("/student-search")
